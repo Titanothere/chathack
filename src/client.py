cb59d5b94e7b7816890cb3eca05e095da11dae2b
Ygge95 <Ygge95@gmail.com>
2015-05-06 19:54:57 +0200

    client.py refactored to OO

--- src/client.py
+++ src/client.py
@@ -3,19 +3,72 @@ __author__ = 'taira'
 import socket
 import sys
 
-s = socket.socket()
+from Pyside.QtCore import *
+from Pyside.QtGui import *
 
-host = socket.gethostname()
-port = 5000
 
-s.connect((host, port))
-print(s.recv(1024).decode()) #what does the number mean?
-name = input("name: ")
-while True:
-    message = input("me: ")
-    message_to_send = name + ": " + message
-    s.send(message_to_send.encode())
-    if message.lower() == "q":
-        break
-s.close()
-sys.exit()
+class Client(QWidget):
+
+    def __init__(self, port, host=socket.gethostname()):
+
+        self.app = QApplication(sys.argv)
+        self.type_label = QLabel("Type:", self)
+        self.type_text_field = QLineEdit(self)
+        self.send_button = QPushButton("Send", self)
+
+        self.sock = socket.socket()
+        self._host = str(host)
+        self._port = port
+        self.address = (self._host, self._port)
+
+        self.message_to_send = None
+        self.name = None
+
+        self.win_height = 400
+        self.win_width = 400
+
+
+        self.initUI()
+
+    def connect(self):
+        self.sock.connect(self.address)
+
+    def end(self):
+        self.sock.close()
+        sys.exit()
+
+    def receive(self):
+        self.sock.recv(1024).decode()
+
+    def send(self):
+        self.sock.send(self.message_to_send.encode())
+
+    def communicate(self):
+        self.connect()
+        self.receive()
+        self.name = input("Name: ")
+
+        while True:
+            message = input("me: ")
+
+            if message.lower() == "q":
+                break
+            self.message_to_send = self.name + ": " + message
+            self.send()
+
+    def initUI(self):
+        QWidget.__init__(self)
+        self.setWindowTitle("chat client")
+        self.setMinimumSize(self.win_width, self.win_height)
+
+        self.type_text_field.setPlaceholderText("Type something...")
+
+        self.show()
+        self.app.exec_()
+
+
+client = Client(port=5000)
+client.connect()
+client.receive()
+client.communicate()
+client.end()
--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ __author__ = 'taira'
 import socket
 import threading
 
+
 def client_thread(connection):
     connection.send("Thank you for connecting".encode())
     while True:
@@ -13,19 +14,20 @@ def client_thread(connection):
     connection.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 host = socket.gethostname()
 port = 5000
 
 s.bind((host, port))
 
-s.listen(10) #what does the number mean?
+s.listen(5)
 
 while True:
     connection, address = s.accept()
     print("connection from " + address[0])
 
-    threading._start_new_thread(client_thread,(connection,))
+    threading._start_new_thread(client_thread, (connection,))
 
 s.close()
 
